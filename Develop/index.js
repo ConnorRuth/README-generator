b7aac1dd67bddfd1ed402350334645b38236702a
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-02-19 12:59:50 -0800

    added arrays for questions and licenses and starting code to functions

---
+++ Develop/index.js
@@ -0,0 +1,44 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const mdGen = require("./utils/generateMarkdown.js");
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {type: 'input',
+    name: 'title',
+    message: 'What title would you like to have?',},
+    {type: 'input',
+    name: 'description',
+    message: 'Describe your project?',},
+    {type: 'input',
+    name: 'install',
+    message: 'What is installation method?',},
+    {type: 'input',
+    name: 'usage',
+    message: 'Describe its usage.',},
+    {type: 'input',
+    name: 'contrib',
+    message: 'what contribution information yould you like to include?',},
+    {type: 'input',
+    name: 'tests',
+    message: 'what info about tests would you like to include?',},
+    {type: 'list',
+    name: 'license',
+    message: 'which license are you using?',
+    choices:["MIT", "Apache 2.0", "GNU general public v3.0", "BSD 2-Clause", "BSD 3-Clause", "Boost Software 1.0", "Creative Commons Zero v1.0", "Eclipse Public 2.0", "Mozilla Public 2.0", " The Unilicense"]}
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data){
+    fs.writeFile("README.md");
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions);
+    writeToFile();
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,45 @@
+const licenses = [{name:"MIT", badge:"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]", link:"https://opensource.org/licenses/MIT"}, 
+{name:"Apache 2.0", badge:"[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]", link:"https://opensource.org/licenses/Apache-2.0"},
+{name:"GNU general public v3.0", badge:"[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]", link:"https://www.gnu.org/licenses/gpl-3.0"}, 
+{name:"BSD 2-Clause", badge:"[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]", link:"https://opensource.org/licenses/BSD-2-Clause"}, 
+{name:"BSD 3-Clause", badge:"[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]", link:"https://opensource.org/licenses/BSD-3-Clause"}, 
+{name:"Boost Software 1.0", badge:"[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]", link:"https://www.boost.org/LICENSE_1_0.txt"}, 
+{name:"Creative Commons Zero v1.0", badge:"[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)]", link:"http://creativecommons.org/publicdomain/zero/1.0/"}, 
+{name:"Eclipse Public 2.0", badge:"[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]", link:"https://opensource.org/licenses/EPL-1.0"}, 
+{name:"Mozilla Public 2.0", badge:"[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]", link:"https://opensource.org/licenses/MPL-2.0"}, 
+{name:"The Unilicense", badge:"[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]", link:"http://unlicense.org/"}];
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(!license){
+    return " ";
+  }else{const selectedLicense = licenses.find(lice => lice.name === license);
+    return selectedLicense.badge;}
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if(!license){
+    return " ";
+  }else{const selectedLicense = licenses.find(lice => lice.name === license);
+    return selectedLicense.link;}
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(!license){
+    return " ";
+  }else{const selectedLicense = licenses.find(lice => lice.name === license);
+    return selectedLicense.badge;}
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
